0f1d8532f30545e8fd9a60d77d255ac7ce433bd2
KDlamini <wilzmar.kwd@gmail.com>
2018-11-27 22:18:21 +0200

    Final product landing page

---
+++ main.js
@@ -0,0 +1,71 @@
+beige = () => {
+    document.getElementById("pro-color-set").style.color = "#272711";
+    document.getElementById("pro-color-mat").style.color = "#272711";
+    document.getElementById("pro-color-set").innerHTML = "Beige";
+    document.getElementById("pro-color-mat").innerHTML = "Beige";
+    document.getElementById("fullset-img").src = "Full bathroom set beige.jpg";
+    document.getElementById("mat-img").src = "Bath mat beige.jpg";
+}
+
+black = () => {
+    document.getElementById("pro-color-set").style.color = "#000000";
+    document.getElementById("pro-color-mat").style.color = "#000000";
+    document.getElementById("pro-color-set").innerHTML = "Black";
+    document.getElementById("pro-color-mat").innerHTML = "Black";
+    document.getElementById("fullset-img").src = "Full bathroom set black.jpg";
+    document.getElementById("mat-img").src = "Bath mat black.jpg";
+}
+
+eggplant = () => {
+    document.getElementById("pro-color-set").style.color = "#800080";
+    document.getElementById("pro-color-mat").style.color = "#800080";
+    document.getElementById("pro-color-set").innerHTML = "Purple";
+    document.getElementById("pro-color-mat").innerHTML = "Purple";
+    document.getElementById("fullset-img").src = "Full bathroom set eggplant.jpg";
+    document.getElementById("mat-img").src = "Bath mat eggplant.jpg";
+}
+
+gray = () => {
+    document.getElementById("pro-color-set").style.color = "#808080";
+    document.getElementById("pro-color-mat").style.color = "#808080";
+    document.getElementById("pro-color-set").innerHTML = "Gray";
+    document.getElementById("pro-color-mat").innerHTML = "Gray";
+    document.getElementById("fullset-img").src = "Full bathroom set gray.jpg";
+    document.getElementById("mat-img").src = "Bath mat gray.jpg";
+}
+
+green = () => {
+    document.getElementById("pro-color-set").style.color = "#008000";
+    document.getElementById("pro-color-mat").style.color = "#008000";
+    document.getElementById("pro-color-set").innerHTML = "Green";
+    document.getElementById("pro-color-mat").innerHTML = "Green";
+    document.getElementById("fullset-img").src = "Full bathroom set gray.jpg";
+    document.getElementById("mat-img").src = "Bath mat green.jpg";
+}
+
+lavender = () => {
+    document.getElementById("pro-color-set").style.color = "#dcb4ee";
+    document.getElementById("pro-color-mat").style.color = "#dcb4ee";
+    document.getElementById("pro-color-set").innerHTML = "Lavender";
+    document.getElementById("pro-color-mat").innerHTML = "Lavender";
+    document.getElementById("fullset-img").src = "Full bathroom set gray.jpg";
+    document.getElementById("mat-img").src = "Bath mat lavender.jpg";
+}
+
+navy = () => {
+    document.getElementById("pro-color-set").style.color = "#01017a";
+    document.getElementById("pro-color-mat").style.color = "#01017a";
+    document.getElementById("pro-color-set").innerHTML = "Navy";
+    document.getElementById("pro-color-mat").innerHTML = "Navy";
+    document.getElementById("fullset-img").src = "Full bathroom set gray.jpg";
+    document.getElementById("mat-img").src = "Bath mat navy.jpg";
+}
+
+teal = () => {
+    document.getElementById("pro-color-set").style.color = "#008080";
+    document.getElementById("pro-color-mat").style.color = "#008080";
+    document.getElementById("pro-color-set").innerHTML = "Teal";
+    document.getElementById("pro-color-mat").innerHTML = "Teal";
+    document.getElementById("fullset-img").src = "Full bathroom set teal.jpg";
+    document.getElementById("mat-img").src = "Bath mat teal.jpg";
+}
